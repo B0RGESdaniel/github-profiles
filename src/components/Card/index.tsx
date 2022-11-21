e4795c809986495eb6a94f524d8792c312258a96
Daniel Borges <dlopesborges@gmail.com>
2022-11-21 01:33:35 -0300

    refactoring

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,6 +1,3 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-
 import { User } from '../../types/User';
 import { useFetchUserDetails } from '../../services/requests/useFetchUserDetails';
 
@@ -17,29 +14,39 @@ export function Card({ userInfo }: UserCardProps) {
 
   return (
     <Container>
+
       <Avatar>
+
         <img src={userDetails?.avatar_url} alt="" />
+
         <div>
           <h2>{userDetails?.name ?? '-'}</h2>
           <a href={userDetails?.html_url} target="_blank">
             {userDetails?.login}
           </a>
         </div>
+
       </Avatar>
+
       <Bio>{userDetails?.bio}</Bio>
+
       <UserStats>
+
         <StatInfo info="followers">
           <h3>{userDetails?.followers ?? 0}</h3>
           <span>Followers</span>
         </StatInfo>
+
         <StatInfo info="following">
           <h3>{userDetails?.following ?? 0}</h3>
           <span>Following</span>
         </StatInfo>
+        
         <StatInfo info="repos">
           <h3>{userDetails?.public_repos ?? 0}</h3>
           <span>Repos</span>
         </StatInfo>
+
       </UserStats>
     </Container>
   );
