81f9615d9826d2b48a69cda330f2590d72c69717
Daniel Borges <dlopesborges@gmail.com>
2022-11-21 01:22:12 -0300

    refactor: Card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,68 +1,59 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-import { User } from "../../pages/Dashboard";
+import { User } from '../../types/User';
+import { useFetchUserDetails } from '../../services/requests/useFetchUserDetails';
 
-import { Container, Avatar, Bio, UserStats, StatInfo } from './styles'
+import { Container, Avatar, Bio, UserStats, StatInfo } from './styles';
 
 interface UserCardProps {
-    userInfo: User;
+  userInfo: User;
 }
 
 const emptyUser = {
-    name: '',
-    login: '',
-    id: 1,
-    avatar_url: '',
-    html_url: '',
-    starred_url: '',
-    bio: '',
-    public_repos: 0,
-    followers: 0,
-    following: 0,
-}
+  name: '',
+  login: '',
+  id: 1,
+  avatar_url: '',
+  html_url: '',
+  starred_url: '',
+  bio: '',
+  public_repos: 0,
+  followers: 0,
+  following: 0,
+};
 
 export function Card({ userInfo }: UserCardProps) {
-    const [user, setUser] = useState<User>(emptyUser)
+  const { data: userDetails, isFetching } = useFetchUserDetails(userInfo?.login);
 
-    async function getUser() {
-        try {
-            const response = await axios.get(`https://api.github.com/users/${userInfo?.login}`)
-            setUser(response.data);
-        } catch (err) {
-            console.log(err)
-        }
+  if (isFetching) return <></>;
 
-    }
-
-    useEffect(() => {
-        getUser()
-    }, [])
-
-    return (
-        <Container>
-            <Avatar>
-                <img src={user.avatar_url} alt="" />
-                <div>
-                    <h2>{user.name ?? '-'}</h2>
-                    <a href={user.html_url} target="_blank">{user.login}</a>
-                </div>
-            </Avatar>
-            <Bio>{user.bio}</Bio>
-            <UserStats>
-                <StatInfo info="followers">
-                    <h3>{user.followers ?? 0}</h3>
-                    <span>Followers</span>
-                </StatInfo>
-                <StatInfo info="following">
-                    <h3>{user.following ?? 0}</h3>
-                    <span>Following</span>
-                </StatInfo>
-                <StatInfo info="repos">
-                    <h3>{user.public_repos ?? 0}</h3>
-                    <span>Repos</span>
-                </StatInfo>
-            </UserStats>
-        </Container>
-    )
+  return (
+    <Container>
+      <Avatar>
+        <img src={userDetails?.avatar_url} alt="" />
+        <div>
+          <h2>{userDetails?.name ?? '-'}</h2>
+          <a href={userDetails?.html_url} target="_blank">
+            {userDetails?.login}
+          </a>
+        </div>
+      </Avatar>
+      <Bio>{userDetails?.bio}</Bio>
+      <UserStats>
+        <StatInfo info="followers">
+          <h3>{userDetails?.followers ?? 0}</h3>
+          <span>Followers</span>
+        </StatInfo>
+        <StatInfo info="following">
+          <h3>{userDetails?.following ?? 0}</h3>
+          <span>Following</span>
+        </StatInfo>
+        <StatInfo info="repos">
+          <h3>{userDetails?.public_repos ?? 0}</h3>
+          <span>Repos</span>
+        </StatInfo>
+      </UserStats>
+    </Container>
+  );
 }
