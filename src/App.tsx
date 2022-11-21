d2009105783b1760afee6c2119afd91599408173
Daniel Borges <dlopesborges@gmail.com>
2022-11-20 23:45:23 -0300

    placing components in App

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Dashboard } from "./pages/Dashboard";
+import { Header } from "./components/Header";
+
+import GlobalStyle from './styles/global'
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <Dashboard />
+      <GlobalStyle />
+    </>
+
+  )
+}
+
