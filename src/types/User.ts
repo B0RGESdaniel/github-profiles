f4e136621392ab6411f9beeef370e3b0a239d407
Daniel Borges <dlopesborges@gmail.com>
2022-11-21 01:22:59 -0300

    create User type

---
+++ src/types/User.ts
@@ -0,0 +1,12 @@
+export type User = {
+  name: string;
+  login?: string;
+  id?: number;
+  avatar_url?: string;
+  html_url?: string;
+  starred_url?: string;
+  bio?: string;
+  public_repos?: number;
+  followers?: number;
+  following?: number;
+};
